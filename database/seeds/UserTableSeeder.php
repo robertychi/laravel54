41ec2d9874a2db5d388ce6447160adae3bb87225
robert <wanstar.lin@gmail.com>
2017-03-14 10:40:27 +0800

    updata seed

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        DB::table('users')->insert([
+            'name' => 'robert',
+            'email' =>'liu@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'lin',
+            'email' =>'lin@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
