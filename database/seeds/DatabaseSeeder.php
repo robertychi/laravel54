176053cf57f379734d8f15767154560907f3ad1d
robert <wanstar.lin@gmail.com>
2017-03-14 10:48:39 +0800

    updata

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //factory(App\Product::class,15)->create();
+        $this->call(UsersTableSeeder::class);
     }
 }
